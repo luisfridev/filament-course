52fadbc51c574a79e7b266855f923e9775ae311a
Luis <luisangeldev@gmail.com>
2024-07-21 20:45:27 -0500

    Adds category_id to $fillable property in Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'price', 'status'];
+    protected $fillable = ['name', 'price', 'status', 'category_id'];
+
+    /**
+     * Get the category that owns the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
