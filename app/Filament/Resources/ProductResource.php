e2eadf0824db232d265c13decd114a11b9d27d0a
Luis <luisangeldev@gmail.com>
2024-07-26 01:54:04 -0500

    Adds TagsRelationManager in ProductResource.php

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -77,7 +77,7 @@ class ProductResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\TagsRelationManager::class,
         ];
     }
 
